94765256a5fd99548db6b4a5c2f346a34341ba6a
GuigaT17 <guigateixeira@protonmail.com>
2022-03-09 07:48:31 -0300

    final commit

--- src/modules/transaction/strategy/ClientWithDiscountStrategy.ts
+++ src/modules/transaction/strategy/ClientWithDiscountStrategy.ts
@@ -1,5 +1,7 @@
 import ICommissionCalculator from "./ICommissionCalculator";
 
+
+// FOR REAL STRATEGY, IT SHOULD HAVE A DB WITH CLEINT IDS THAT HAVE DISCOUNT, SO ITS NOT HARD CODED
 export default class ClientWithDiscountStrategy implements ICommissionCalculator {
     calculateCommision(client_id: number, amount: number, date: Date) {
         if(client_id == 42) {
