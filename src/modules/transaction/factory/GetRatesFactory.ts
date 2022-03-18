adada7a70a20be20f8ced4d8f958d5e2722d1248
GuigaT17 <guigateixeira@protonmail.com>
2022-03-18 12:28:25 -0300

    updating hardcoded url

--- src/modules/transaction/factory/GetRatesFactory.ts
+++ src/modules/transaction/factory/GetRatesFactory.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
+import { URLConfig } from "src/config/URLConfig";
+
+const urlconfig: URLConfig = require("resources/urlconfig.json");
 
 export default class GetRatesFactory {
     public async getRate(currency: string){
-        const response = await axios.get('https://api.exchangerate.host/2021-01-01');
+        const response = await axios.get(urlconfig.url);
         const rates = response.data.rates;
         return response.data.rates[currency];
     }
